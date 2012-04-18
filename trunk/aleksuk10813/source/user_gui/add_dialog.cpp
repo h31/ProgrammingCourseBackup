2c384cfc7390fba6e61bf344f8996ff3fe432bc9
ArtyomAleksuk <ArtyomAleksuk@fd3ab589-dedd-4ea3-9a7c-0c2449daa773>
2012-04-18 20:50:34 +0000

    Перешёл на Designer - намного удобнее в моём случае

---
+++ trunk/aleksuk10813/source/user_gui/add_dialog.cpp
@@ -0,0 +1,20 @@
+#include "add_dialog.h"
+#include "ui_add_dialog.h"
+
+AddDialog::AddDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::AddDialog)
+{
+    ui->setupUi(this);
+}
+
+AddDialog::~AddDialog()
+{
+    delete ui;
+}
+
+void AddDialog::on_buttonBox_accepted()
+{
+    address = ui->address->text();
+    protocol = ui->protocol->text();
+}
