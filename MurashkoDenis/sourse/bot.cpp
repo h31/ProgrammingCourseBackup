5072a9dab7767d08febc3ce43657dafd6d87a53a
ArtyomAleksuk <ArtyomAleksuk@fd3ab589-dedd-4ea3-9a7c-0c2449daa773>
2012-05-30 20:39:33 +0000

    По просьбам телезрителей :)

--- MurashkoDenis/sourse/bot.cpp
+++ MurashkoDenis/sourse/bot.cpp
@@ -12,8 +12,10 @@ Bot::Bot()
 
 void Bot::draw(QPainter &painter)
 {
+    painter.setBrush(Qt::red);
     painter.setPen(Qt::red);
     painter.drawEllipse(QRect(koordsb.x-r,koordsb.y-r,2*r,2*r));
+    painter.setPen(Qt::white);
     painter.drawEllipse(QRect(koordsb.x+vecb.x-5,koordsb.y+vecb.y-5,10,10));
 
 }
